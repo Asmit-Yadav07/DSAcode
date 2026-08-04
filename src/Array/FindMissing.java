574a2a4cf1980a825d7494380d0dd78464c02895
Asmit-Yadav07 <suspiciousfun987@gmail.com>
2026-08-04 22:51:26 +0530

    Find Missing number in array

---
+++ src/Array/FindMissing.java
@@ -0,0 +1,25 @@
+// You are given an array arr[] of size n - 1 that contains distinct integers in the range from 1 to n (inclusive). This array represents a permutation of the integers from 1 to n with one element missing. Your task is to identify and return the missing element.
+
+// Examples:
+// Input: arr[] = [1, 2, 3, 5]
+// Output: 4
+// Explanation: All the numbers from 1 to 5 are present except 4.
+
+package Array;
+
+public class FindMissing {
+    static int missingNum(int arr[]) {
+        // code here
+       int n=arr.length+1;
+       int expectedSum=n*(n+1)/2;
+       int sum=0;
+       for(int i =0;i<n-1;i++){
+           sum+=arr[i];
+       }
+       return expectedSum-sum;
+    }
+    public static void main(String[] args) {
+        int [] n={1,2,4,5};
+        System.out.println(missingNum(n));
+    }
+}
